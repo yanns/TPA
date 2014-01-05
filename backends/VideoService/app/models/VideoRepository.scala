7beb3faf1f19dbe2cd6d11cfe95ed86cd78cf49a
Yann Simon <yann.simon.fr@gmail.com>
2014-01-05 16:49:34 +0100

    add videos

--- backends/VideoService/app/models/VideoRepository.scala
+++ backends/VideoService/app/models/VideoRepository.scala
@@ -4,11 +4,12 @@ package models
 object VideoRepository {
 
   private val videos = Seq(
-    Video(VideoId(1), "dunk", Seq(PlayerId(1))),
-    Video(VideoId(2), "pass", Seq(PlayerId(2), PlayerId(3)))
+    Video(VideoId(1), "awesome powerful dunk", Seq(PlayerId(1))),
+    Video(VideoId(2), "best try ever", Seq(PlayerId(2))),
+    Video(VideoId(3), "defense beautiful move", Seq(PlayerId(3), PlayerId(2)))
   )
 
-  def topVideos() = videos.slice(0, 2)
+  def topVideos() = videos.slice(0, 3)
 
   def getVideoById(id: VideoId): Option[Video] = videos.find(_.id == id)
 
