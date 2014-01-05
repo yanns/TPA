915df8883ac1c5100a1ec8d15aa0d864c9cbe0c8
Yann Simon <yann.simon.fr@gmail.com>
2014-01-05 11:37:21 +0100

    video streaming more robust

--- backends/VideoService/app/controllers/VideoResource.scala
+++ backends/VideoService/app/controllers/VideoResource.scala
@@ -2,53 +2,65 @@ package controllers
 
 import java.io.{FileInputStream, File}
 import models.{VideoRepository, VideoId, PlayerId}
-import play.api.{Logger, Play}
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.Json
-import play.api.mvc.{SimpleResult, ResponseHeader, Action, Controller}
+import play.api.mvc._
+import play.api.{Logger, Play}
 
 object VideoResource extends Controller {
 
   def videoStream(id: VideoId) = Action { request =>
     Logger.debug(s"streaming video $id")
-    VideoRepository.getVideoById(id) match {
-      case Some(v) => {
-        val file = new File(Play.application.getFile("/public/videos"), s"video-${v.id}.mp4")
-        if (file.exists()) {
-          request.headers.get(RANGE) match {
-            case None => Ok.sendFile(file).as("video/mp4")
-            case Some(value) => {
-              val (start, end) = value.substring("bytes=".length).split("-") match {
-                case x if x.length == 1 => x.head.toLong -> (file.length() - 1)
-                case x => x(0).toLong -> x(1).toLong
-              }
 
+    val streamResult = VideoRepository
+      .getVideoById(id)
+      .map { v => new File(Play.application.getFile("/public/videos"), s"video-${v.id}.mp4") }
+      .filter { f => f.isFile && f.exists() }
+      .map { file =>
+        request.headers.get(RANGE) match {
+
+          // response without partial content
+          case None => Ok.sendFile(file).as("video/mp4").withHeaders(ACCEPT_RANGES -> "bytes")
+
+          case Some(value) if !value.contains("bytes=") => BadRequest("bad 'Range' header")
+
+          // response with partial content
+          case Some(value) => {
+            val fileLength = file.length()
+            val (start, end) = value.substring("bytes=".length).split("-") match {
+              case x if x.length == 1 => x.head.toLong -> (fileLength - 1)
+              case x => x(0).toLong -> x(1).toLong
+            }
+
+            if (start < 0 || end < 0 || start >= fileLength || end >= fileLength) {
+              Status(REQUESTED_RANGE_NOT_SATISFIABLE).withHeaders(CONTENT_RANGE -> "bytes */%d".format(fileLength))
+            } else {
               val stream = new FileInputStream(file)
+
               stream.skip(start)
 
+              // TODO do we need connection = HttpConnection.KeepAlive?
               SimpleResult(
                 header = ResponseHeader(PARTIAL_CONTENT,
                   Map(
                     CONNECTION -> "keep-alive",
                     ACCEPT_RANGES -> "bytes",
-                    CONTENT_RANGE -> "bytes %d-%d/%d".format(start, end, file.length()),
+                    CONTENT_RANGE -> "bytes %d-%d/%d".format(start, end, fileLength),
                     CONTENT_LENGTH -> (end - start + 1).toString,
                     CONTENT_TYPE -> "video/mp4"
                   )
                 ),
-                body = Enumerator.fromStream(stream)
+                body = Enumerator.fromStream(stream),
+                connection = HttpConnection.KeepAlive
               )
             }
           }
-
-        } else {
-          NotFound
         }
       }
-      case None => NotFound
-    }
+
+    streamResult.getOrElse(NotFound)
   }
 
   def topVideos() = Action {
