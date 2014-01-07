78ff13a3c28274498150abe564748b759d51910b
Yann Simon <yann.simon.fr@gmail.com>
2014-01-07 21:30:34 +0100

    trait is already abstract

--- frontend/TBA_04/test/components/MockEnvironment.scala
+++ frontend/TBA_04/test/components/MockEnvironment.scala
@@ -2,7 +2,7 @@ package components
 
 import org.specs2.mock.Mockito
 
-abstract trait MockEnvironment extends Registry with Mockito {
+trait MockEnvironment extends Registry with Mockito {
   override val httpClient = mock[HttpClient]
   override val playerGateway = mock[PlayerGateway]
   override val videoGateway = mock[VideoGateway]
